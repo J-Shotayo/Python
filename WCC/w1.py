fa15be8b4fe00e00adb31e5f5958944b9b7e4dcd
J-Shotayo <damolatunde17@gmail.com>
2025-03-12 16:35:13 +0100

    Completed Weekly Challenge 1

---
+++ WCC/w1.py
@@ -0,0 +1,9 @@
+# Program to ask for user's name and favorite color, then print a personalized greeting
+# Ask for user's name
+name = input("What is your name? ")
+
+# Ask for user's favorite color
+color = input("What is your favorite color? ")
+
+# Print personalized greeting
+print(f"Hello, {name}! Your favorite color, {color}, is awesome!")
