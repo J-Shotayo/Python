77f3787721042943a3a157620d7368c40c57d4b1
J-Shotayo <damolatunde17@gmail.com>
2025-03-24 09:52:56 +0100

    Completed Weekly coding challenge number 3

---
+++ WCC/w2.py
@@ -0,0 +1,11 @@
+# Create an empty dictionary
+person = {}
+
+# Get user input
+person["name"] = input("Enter your name: ")
+person["age"] = int(input("Enter your age: "))  # Convert age to an integer
+person["favorite_color"] = input("Enter your favorite color: ")
+
+# Print the dictionary
+print("\nPerson Information:")
+print(person)
